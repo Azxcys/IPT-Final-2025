3b5ba89055f165ecc15e80f0da9bc7ce6d8f21d2
Magsayo, Henry Ken <henrykenm@yahoo.com>
2025-05-17 21:56:41 +0800

    Checking and Finalizing Ver 1.9

--- backend/src/controllers/departmentController.ts
+++ backend/src/controllers/departmentController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import pool from '../config/database';
 import { v4 as uuidv4 } from 'uuid';
-import { RowDataPacket } from 'mysql2';
+import type { RowDataPacket } from 'mysql2/promise';
 
 interface DepartmentRow extends RowDataPacket {
   id: string;
