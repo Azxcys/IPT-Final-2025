61ffe87a21bf95c976f3aeab2dcc9a69732bcd41
Magsayo, Henry Ken <henrykenm@yahoo.com>
2025-05-17 21:30:09 +0800

    Checking and Finalizing Ver 1.3

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,9 +1,9 @@
-import mysql, { Pool, PoolOptions } from 'mysql2/promise';
+import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const poolConfig: PoolOptions = {
+const pool = mysql.createPool({
   host: process.env.DB_HOST || '153.92.15.31',
   user: process.env.DB_USER || 'u875409848_hmagsayo',
   password: process.env.DB_PASSWORD || '9T2Z5$3UKkgSYzE',
@@ -11,8 +11,6 @@ const poolConfig: PoolOptions = {
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
-};
-
-const pool: Pool = mysql.createPool(poolConfig);
+});
 
 export default pool; 
